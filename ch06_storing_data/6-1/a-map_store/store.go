c572003f76c433fa29f7db2a9712a62efef1fbf9
sam <sam@163.com>
2020-06-29 13:45:07 +0800

    ch06

--- ch06_storing_data/6-1/a-map_store/store.go
+++ ch06_storing_data/6-1/a-map_store/store.go
@@ -1,8 +1,7 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
 //所谓内存存储，即定义一些数据结构，数组切片，图或者其他自定义结构，
 //把需要持久化的数据存储在这些数据结构中。使用数据的时候可以直接操作这些结构。
 //使用映射map作为结构容器的例子
@@ -13,12 +12,14 @@ type Post struct {
 	Author  string
 }
 
-//我们定义了两个map的结构PostById，PostByAuthor，store方法会把post数据存入这两个map结构中。当需要数据的时候，再从这两个内存结构读取即可。
+//我们定义了两个map的结构PostById，PostByAuthor
+//store方法会把post数据存入这两个map结构中。当需要数据的时候，再从这两个内存结构读取即可。
 //内存持久化比较简单，严格来说这也不算是持久化，程序退出会清空内存，所保存的数据也会消失。这种持久化只是相对程序运行时而言。
 //想要程序退出重启还能读取所存储的数据，这时就得依赖文件或者数据库（非内存数据库）。
 
 var PostById map[int]*Post
-var PostsByAuthor map[string][]*Post //这个value是个切片额
+var PostsByAuthor map[string][]*Post //这个value是个切片额,因为每个作者可能有多个帖子
+
 
 
 func store(post Post) {
@@ -28,39 +29,28 @@ func store(post Post) {
 
 func main() {
 
+	//初始化上述的两个map，否则无法真正使用
 	PostById = make(map[int]*Post)
 	PostsByAuthor = make(map[string][]*Post)
-
+    //初始化几个帖子实例
 	post1 := Post{Id: 1, Content: "Hello World!", Author: "Sau Sheong"}
 	post2 := Post{Id: 2, Content: "Bonjour Monde!", Author: "Pierre"}
 	post3 := Post{Id: 3, Content: "Hola Mundo!", Author: "Pedro"}
 	post4 := Post{Id: 4, Content: "Greetings Earthlings!", Author: "Sau Sheong"}
-
+	//存储
 	store(post1)
 	store(post2)
 	store(post3)
 	store(post4)
+	//打印
+	fmt.Printf("%+v\r\n",PostById)
+	fmt.Printf("%+v\r\n",PostById[1])
+	fmt.Printf("%+v\r\n",PostsByAuthor)
+	fmt.Printf("%+v\r\n",PostsByAuthor["Pierre"])
 
-	//map[1:0xc0000641e0 2:0xc000064210 3:0xc000064240 4:0xc000064270]
-	//map[Pedro:[0xc000064240] Pierre:[0xc000064210] Sau Sheong:[0xc0000641e0 0xc000064270]]
 
-	fmt.Println(PostById)
-	fmt.Println(PostsByAuthor)
 
-	fmt.Println(PostById[1])
-	fmt.Println(PostById[2])
 
-	//PostsByAuthor["Sau Sheong"]是个切片
-	for _, post := range PostsByAuthor["Sau Sheong"] {
-		fmt.Println(post)
-	}
-	//PostsByAuthor["Pedro"]是个切片
-	for _, post := range PostsByAuthor["Pedro"] {
-		fmt.Println(post)
-	}
-	//&{1 Hello World! Sau Sheong}
-	//&{2 Bonjour Monde! Pierre}
-	//&{1 Hello World! Sau Sheong}
-	//&{4 Greetings Earthlings! Sau Sheong}
-	//&{3 Hola Mundo! Pedro}
+
+
 }
