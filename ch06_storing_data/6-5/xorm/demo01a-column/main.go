75856442f58ce3aae40f0cba1206756b8516b210
sam <sam@163.com>
2020-08-18 17:38:54 +0800

    xorm

--- ch06_storing_data/6-5/xorm/demo01a-column/main.go
+++ ch06_storing_data/6-5/xorm/demo01a-column/main.go
@@ -7,22 +7,33 @@ import (
 	"xorm.io/xorm"
 )
 
+//联合普通索引
+//唯一索引
+//普通索引
+//默认值
+//非空
+//...
 
+
+
+//使用 xorm:"'column_name'"可以使该 field 对应的 Column 名称为指定名称。这里使用两个单引号将 Column 名称括起来是为了防止名称冲突，
+//因为我们在Tag中还可以对这个Column进行更多的定义。如果名称不冲突的情况，单引号也可以不使用。
 
 type Student struct {
-	Id        int64 `xorm:"pk autoincr"` // 注：使用getOne 或者ID() 需要设置主键
-	Age      int
-	Name     string
-	ImageUrl  string `xorm:"'img_url'"`  //使用 xorm:"'column_name'"可以使该 field 对应的 Column 名称为指定名称,注意里面的单引号额
-	Url       string
-	Status    uint8
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	Id        int64 `xorm:"pk autoincr"`
+	Age      int     `xorm:"notnull default 0 comment('年纪')"`
+	Name     string  `xorm:"varchar(255) notnull unique  default '' comment('姓名')"`
+	Url       string  `xorm:"index notnull index default '' comment('地址')"`
+	Status    uint8    `xorm:"notnull default 0 comment('状态')"`
+	Version   int64   `xorm:"version"`
+	SpecialCreatedAt   int64 `xorm:"created"`
+	CreatedAt time.Time `xorm:"created"` //todo 注意不要用单引号括起来created,单引号是用来括字段名，当字段名与行为符冲突的时候额
+	UpdatedAt time.Time  `xorm:"updated"`
 	DeletedAt time.Time `xorm:"'deleted'"` // 此特性会激发软删除
 }
 
 func (m *Student) TableName() string {
-	return "student1"
+	return "student3"
 }
 
 func main() {
@@ -39,5 +50,13 @@ func main() {
 	if err !=nil{
 		log.Fatal(err)
 	}
+
+	//插入
+	stu01:=&Student{
+		Name:"sam",
+		Age:11,
+		Status:1,
+	}
+	engine.Insert(stu01)
 
 }
