3db4d93afb2c841652a8025a3db144ba7df0404a
sam <sam@163.com>
2021-01-13 10:57:19 +0800

    ch04

--- ch04_processing_requests/4-1/d02_header/server.go
+++ ch04_processing_requests/4-1/d02_header/server.go
@@ -8,9 +8,10 @@ import (
 //读取请求首部
 //一个Header类型的实例就是一个映射，这个映射的键为字符串 ，而键的值则是由任意多个字符串组成的切片。
 //即二维数组，键是二维数组的key，值是二维数组的元素。
+//type Header map[string][]string
+//将map[string][]string自定义成了Header类型，本质就是可变元素的关联数组。当一个数组的元素是一维数组，它自然就是二维数组了。
 func headers(w http.ResponseWriter, r *http.Request) {
-	//type Header map[string][]string
-	//将map[string][]string自定义成了Header类型，本质就是可变元素的关联数组。当一个数组的元素是一维数组，它自然就是二维数组了。
+
 	h := r.Header
     //获取某个具体的首部值
     //h := r.Header["Accept-Encoding"] //返回的是切片
